ab4839e463bdee9fdf6c34a6020cfeea3a35c10d
antuelo <antuel@outlook.com>
2025-03-30 11:53:44 +0200

    valgrid comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: anoviedo <antuel@outlook.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 14:30:00 by anoviedo          #+#    #+#             */
-/*   Updated: 2025/03/30 11:46:53 by anoviedo         ###   ########.fr       */
+/*   Updated: 2025/03/30 11:52:47 by anoviedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,3 +95,4 @@ int	main(int argc, char **argv)
 	free_map(game.map);
 	return (0);
 }
+/*valgrind --leak-check=full ./so_long maps/map1.ber*/
